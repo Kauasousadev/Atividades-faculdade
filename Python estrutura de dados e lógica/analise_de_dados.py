bc7dc583bba116d977b7bfd54eeac5fdce3e2061
Kauã Batista de Sousa <kaka021510@gmail.com>
2024-08-05 14:24:08 -0300

    Update analise_de_dados.py
    teste de branch e merge

--- Python estrutura de dados e lógica/analise_de_dados.py
+++ Python estrutura de dados e lógica/analise_de_dados.py
@@ -2,7 +2,7 @@ import pandas
 
 array = [1,4,3]
 
-agenda = {"Segunda": "Reunião", "Terça": "Encontro"}
+agenda = {"Segunda": "Reunião", "Terça": "Encontro", "Quarta": "Entrevista"}
 
 var = pandas.Series(agenda)
 
